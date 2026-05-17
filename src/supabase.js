a7d4cf8cbd4054f84f95d052375d6c944037216e
ThomasSSRR <thomas.hussherr@gmail.com>
2026-05-17 22:48:01 +0200

    hardcode supabase keys

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://bvrzuaocnsyjfwgtbyku.supabase.co'
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseAnonKey = 'sb_publishable_7Qa0qdiYmIMmW6LtOOZKig_Vu2ROQh7'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
